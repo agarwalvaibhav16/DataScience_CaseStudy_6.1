d80cd596fcbf26a13c945867550e47b041411dda
Vaibhav Agarwal <agarwalvaibhav16@gmail.com>
2018-07-08 03:36:07 -0400

    Case Study 6.1 Newyork Taxi
    Case Study 6.1 Newyork Taxi Data Set

---
+++ utils.py
@@ -0,0 +1,90 @@
+import featuretools as ft
+import numpy as np
+import pandas as pd
+from featuretools.primitives import (Day, Hour, Max, Mean, Min, Minute, Month,
+                                     Skew, Std, Week, Weekday, Weekend)
+from sklearn.cluster import KMeans
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import Imputer
+
+# set global random seed
+np.random.seed(40)
+
+
+####################
+# Case Study Utils #
+####################
+
+def preview(df, n=5):
+    """return n rows that have fewest number of nulls"""
+    order = df.isnull().sum(axis=1).sort_values(kind='mergesort').head(n).index
+    return df.loc[order]
+
+
+def feature_importances(model, feature_names, n=10):
+    importances = model.feature_importances_
+    zipped = sorted(zip(feature_names, importances), key=lambda x: -x[1])
+    for i, f in enumerate(zipped[:n]):
+        print("%d: Feature: %s, %.3f" % (i + 1, f[0], f[1]))
+
+
+def get_train_test_fm(feature_matrix, percentage):
+    nrows = feature_matrix.shape[0]
+    head = int(nrows * percentage)
+    tail = nrows - head
+    X_train = feature_matrix.head(head)
+    y_train = X_train['trip_duration']
+    X_train = X_train.drop(['trip_duration'], axis=1)
+    imp = Imputer()
+    X_train = imp.fit_transform(X_train)
+    X_test = feature_matrix.tail(tail)
+    y_test = X_test['trip_duration']
+    X_test = X_test.drop(['trip_duration'], axis=1)
+    X_test = imp.transform(X_test)
+
+    return (X_train, y_train, X_test, y_test)
+
+
+##################
+# Case Study 6.1 #
+##################
+
+def column_string(n):
+    string = ""
+    while n > 0:
+        n, remainder = divmod(n - 1, 26)
+        string = chr(65 + remainder) + string
+    return string
+
+
+def load_nyc_taxi_data():
+    trips = pd.read_pickle('trips.pkl')
+    trips["payment_type"] = trips["payment_type"].apply(str)
+    trips = trips.dropna(axis=0, how='any', subset=['trip_duration'])
+
+    pickup_neighborhoods = pd.read_csv(
+        "pickup_neighborhoods.csv", encoding='utf-8')
+    dropoff_neighborhoods = pd.read_csv(
+        "dropoff_neighborhoods.csv", encoding='utf-8')
+
+    return trips, pickup_neighborhoods, dropoff_neighborhoods
+
+
+def compute_features(features, cutoff_time):
+    # shuffle so we don't see encoded features in the front or backs
+
+    np.random.shuffle(features)
+    feature_matrix = ft.calculate_feature_matrix(features,
+                                                 cutoff_time=cutoff_time,
+                                                 approximate='36d',
+                                                 verbose=True)
+    print("Finishing computing...")
+    feature_matrix, features = ft.encode_features(feature_matrix, features,
+                                                  to_encode=[
+                                                      "pickup_neighborhood",
+                                                      "dropoff_neighborhood"],
+                                                  include_unknown=False)
+    return feature_matrix
+
+
+
